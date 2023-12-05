4b98d54adf85d4c86713ca4cedc13d1d254cdfd5
Nicholas Murray <nmurr13@protonmail.com>
2023-12-05 00:18:48 -0800

    Commit 1 - I added my first element and it was fun

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
-  
+const $container= $('#container')
+const $h1= $('<h1>Hogwarts</h1>').appendTo('#container')
