b558223567b39b2995ff931d5307500b1c88eab8
Nicholas Murray <nmurr13@protonmail.com>
2023-12-05 00:34:20 -0800

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const $container= $('#container')
 const $h1= $('<h1>Hogwarts</h1>').appendTo('#container')
+const $h2= $('<h2>Nicholas Murray</h2>').appendTo('#container')
+const $h3= $('<h3>Hufflepuff</h3>').appendTo('#container')
+const $h4= $('<h4>Junior</h4>').appendTo('#container')
+$h4.addClass("cat")
+const $h4wand= $('<h4>Holly Wand with Unicorn Hair Core</h4>').appendTo('#container')
+
